16ca44431549369be0eb96e6774ff34f28487388
Akhmed0 <137339971+Akhmed0@users.noreply.github.com>
2023-08-23 20:06:31 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,73 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите выражение: ");
-        String expression = scanner.nextLine();
-
-        try {
-            String result = calculateExpression(expression);
-            System.out.println("Результат: " + result);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка: " + e.getMessage());
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String exp = scn.nextLine();
+        char action;
+        String[] data;
+        if (exp.contains(" + ")) {
+            data = exp.split(" \\+ ");
+            action = '+';
+        } else if (exp.contains(" - ")) {
+            data = exp.split(" - ");
+            action = '-';
+        } else if (exp.contains(" * ")) {
+            data = exp.split(" \\* ");
+            action = '*';
+        } else if (exp.contains(" / ")) {
+            data = exp.split(" / ");
+            action = '/';
+        } else {
+            throw new Exception("Некорректный знак действия");
         }
+        if (action == '*' || action == '/') {
+            if (data[1].contains("\"")) throw new Exception("Строчку можно делить или умножать только на число");
+        }
+        for (int i = 0; i < data.length; i++) {
+            data[i] = data[i].replace("\"", "");
+            if (data[i].length() > 10) {
+                throw new IllegalArgumentException("Калькулятор может принимать строки длиной от 1 до 10 символов");
+            }
+        }
+
+        if (action == '+') {
+            printInQuotes(data[0] + data[1]);
+        } else if (action == '*') {
+            if (!data[1].matches("[1-9]|10")) {
+                throw new IllegalArgumentException("числа дожны быть от 1 до 10");
+            }
+            int multiplier = Integer.parseInt(data[1]);
+            String result = "";
+            for (int i = 0; i < multiplier; i++) {
+                result += data[0];
+            }
+            printInQuotes(result);
+        } else if (action == '-') {
+            int index = data[0].indexOf(data[1]);
+            if (index == -1) {
+                printInQuotes(data[0]);
+            } else {
+                String result = data[0].substring(0, index);
+                result += data[0].substring(index + data[1].length());
+                printInQuotes(result);
+            }
+        } else {
+            if (!data[1].matches("[1-9]|10")) {
+                throw new IllegalArgumentException("числа дожны быть от 1 до 10");
+            }
+            int newLen = data[0].length() / Integer.parseInt(data[1]);
+            String result = data[0].substring(0, newLen);
+            printInQuotes(result);
+        }
+
     }
 
-    public static String calculateExpression(String expression) {
-        String[] parts = expression.split(" ");
-
-        if (parts.length != 3) {
-            throw new IllegalArgumentException("Некорректное выражение! Проверьте правильность ввода.");
+    static void printInQuotes(String text) throws Exception {
+        if (text.length() > 40) {
+            System.out.println("\"" + text.substring(0, 40) + "..." + "\"");
+        } else {
+            System.out.println("\"" + text + "\"");
         }
-
-        String operand1 = parts[0];
-        String operator = parts[1];
-        String operand2 = parts[2];
-
-        if (!operand1.matches("\".*\"") || !operand2.matches("\".*\"")) {
-            throw new IllegalArgumentException("Первый и второй операнды должны быть строками, заключенными в двойные кавычки!");
-        }
-
-        operand1 = operand1.substring(1, operand1.length() - 1); // Удаляем кавычки из операндов-строк
-        operand2 = operand2.substring(1, operand2.length() - 1);
-
-        switch (operator) {
-            case "+":
-                return operand1 + operand2;
-            case "-":
-                return operand1.replace(operand2, "");
-            case "*":
-                int repeatCount = Integer.parseInt(operand2);
-                return repeatString(operand1, repeatCount);
-            case "/":
-                int divisor = Integer.parseInt(operand2);
-                int divideLength = operand1.length() / divisor;
-                return operand1.substring(0, divideLength);
-            default:
-                throw new IllegalArgumentException("Неподдерживаемая операция! Доступны операции: +, -, *, /");
-        }
-    }
-
-    public static String repeatString(String str, int repeatCount) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < repeatCount; i++) {
-            result.append(str);
-        }
-        return result.toString();
     }
 }
